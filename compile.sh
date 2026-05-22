562d54d056f186c1e45c7321f54573b9a08f4483
C-r0 <146779565+C-r0@users.noreply.github.com>
2026-05-22 12:34:27 +0000

    Update compile script to handle additional argument

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 nasm -f elf64 src/compiler.asm -o build/compiler.o
 ld build/compiler.o -o build/compiler
-./build/compiler $1 $2
-nasm -f elf64 src/loader.asm -o build/loader.o
-ld build/loader.o -o $3
+./build/compiler $1 $2 $3
 echo "----------------------------"
-./$3
+chmod +x $2
+./$2
